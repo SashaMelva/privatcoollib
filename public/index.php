0af5e35bbe49847c6872aa78ef18db00b74d8f9a
SashaMelva <sa1524517@gmail.com>
2023-03-21 15:12:59 +0300

    first

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+use App\ExchangedAmount;
+
+$amount = new ExchangedAmount("USD", "UAH", 100);
+var_dump($amount->toDecimal());
